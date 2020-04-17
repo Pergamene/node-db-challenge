a03e1de8e4a6a2536cd6da13f2b260a2796e2948
Pergamene <smartz373@gmail.com>
2020-04-17 11:59:06 -0600

    fixed seed data

--- data/seeds/01-projects.js
+++ data/seeds/01-projects.js
@@ -3,7 +3,7 @@ exports.seed = function(knex) {
     .then(function () {
       return knex('Projects').insert([
         {name: 'p1', description: 'd1', completed: true},
-        {name: 'p2', description: 'd2'},
+        {name: 'p2', description: 'd2', completed: false},
         {name: 'p3', completed: false},
       ]);
     });
