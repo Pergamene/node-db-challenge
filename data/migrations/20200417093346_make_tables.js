55ad48a150092d5b65b3534c319983a911d7923c
Pergamene <smartz373@gmail.com>
2020-04-17 09:48:44 -0600

    db created

---
+++ data/migrations/20200417093346_make_tables.js
@@ -0,0 +1,41 @@
+exports.up = function(knex) {
+  return knex.schema
+    // projects
+    .createTable('Projects', tbl => {
+      tbl.increments('id');
+      tbl.string('name').notNullable().unique().index();
+      tbl.text('description');
+      tbl.boolean('completed').defaultTo(false);
+    })
+
+    // tasks
+    .createTable('Tasks', tbl => {
+      tbl.increments('id');
+      tbl.integer('project_id').notNullable().references('id').inTable('Projects').index();
+      tbl.text('description').notNullable();
+      tbl.text('notes');
+      tbl.boolean('completed').defaultTo(false);
+    })
+    
+    // resources
+    .createTable('Resources', tbl => {
+      tbl.increments('id');
+      tbl.string('name').notNullable().unique().index();
+      tbl.text('description');
+    })
+
+    // resource details
+    .createTable('ResourceDetails', tbl => {
+      tbl.increments('id');
+      tbl.integer('project_id').notNullable().references('id').inTable('Projects').index();
+      tbl.integer('resource_id').notNullable().references('id').inTable('Resources').index();
+    });
+};
+
+exports.down = function(knex) {
+  return knex.schema
+    .dropTableIfExists('Projects')
+    .dropTableIfExists('Tasks')
+    .dropTableIfExists('Resources')
+    .dropTableIfExists('ResourceDetails');
+};
